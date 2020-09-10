81ea4398e416a2e3f8b896128921d4734802afda
Jefferson Freitas <ajeffsoft@gmail.com>
2020-09-09 21:30:02 -0300

    First commit

---
+++ src/test/java/dev/jeffersonfreitas/finances/controller/StateControllerTest.java
@@ -0,0 +1,17 @@
+package dev.jeffersonfreitas.finances.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class StateControllerTest {
+	
+	
+
+}
